268fdb423847d0cd74b30f62a891df4ce8e9d9fe
AustinSefton <AustinSefton@FICO.com>
2019-07-21 21:11:44 -0700

    fixes unit tests and adds check for NaN

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
 /*
 
-Indicies of Sums
+Indices of Sums
 
 Write a function in JavaScript that accepts an array of integers and a number X as parameters, 
 when invoked, returns an array of unique indices of two numbers whose sum is equal to X.
@@ -12,27 +11,32 @@ Pairs of indices: [0, 8], [1, 7], [2, 6], [4, 5]
 
  */
 
-const indiciesOfSums = (arr, sum, results = [], startingIndex = 0) => {
-    if (!Array.isArray(arr) || typeof sum !== 'number')
+const indicesOfSums = (arr, sum, results = [], startingIndex = 0) => {
+    if (!Array.isArray(arr) || (typeof sum !== 'number' || isNaN(sum)))
         throw new Error("Argument 1 should be an array, argument 2 should be a number")
 
-    if (arr.length - 1 < startingIndex) return results;
+    // Base case: once we're past the last index, return results
+    if (arr.length - 1 < startingIndex) 
+        return results;
 
-    const newIndicies = arr.reduce((acc, curr, i) => {
-        if (i < startingIndex + 1)
+    // iterate through and check for sums that equal given sum
+    const newIndices = arr.reduce((acc, curr, i) => {
+        if (i < startingIndex + 1) 
             return acc;
-            
+
         const currSum = arr[startingIndex] + curr;
 
-        if (currSum === sum)
+        if (currSum === sum) 
             acc.push([startingIndex, i]);
 
         return acc;
+
     }, [])
 
-    const newResults = [...results, ...newIndicies];
+    // add the new pairs of indices to the old list
+    const newResults = [...results, ...newIndices];
 
-    return indiciesOfSums(arr, sum, newResults, startingIndex + 1);     
+    return indicesOfSums(arr, sum, newResults, startingIndex + 1);
 }
 
-export default indiciesOfSums;
+export default indicesOfSums;
--- test/test.js
+++ test/test.js
@@ -1,53 +1,43 @@
-
 import { assert, expect } from "chai"
-import indiciesOfSums from '../src/index.js'
+import indicesOfSums from '../src/index.js'
 
-describe('Robin Healthcate Challenge', function() {
-    describe('indiciesOfSums()', function() {
+describe('Robin Healthcare Challenge', function() {
+    describe('indicesOfSums()', function() {
         it('Should return an array', function() {
-            assert.equal(Array.isArray(indiciesOfSums([], 0)), true)
-            assert.equal(Array.isArray(indiciesOfSums([1,2,3,4], 5)), true)
+            assert.equal(Array.isArray(indicesOfSums([], 0)), true);
+            assert.equal(Array.isArray(indicesOfSums([1,2,3,4], 5)), true);
         })
         it('Should throw an error if Array is not first argument', function() {
-            const args = ['', true, false, {}, 4, NaN, undefined]
+            const args = ['', true, false, {}, 4, NaN, undefined];
             args.forEach((arg) => {
                 try {
-                    indiciesOfSums(arg, 0);
+                    indicesOfSums(arg, 0);
+                    assert.equal(true, false)
                 } catch(e) {
                     expect(e.message).to.equal('Argument 1 should be an array, argument 2 should be a number');
                 }
             })
         })
         it('Should throw an error if number is not second argument', function() {
-            const args = ['', true, false, {}, [], NaN, undefined]
+            const args = ['', true, false, {}, [], NaN, undefined];
             args.forEach((arg) => {
                 try {
-                    indiciesOfSums([], arg);
+                    indicesOfSums([], arg);
+                    assert.equal(true, false)
                 } catch(e) {
                     expect(e.message).to.equal('Argument 1 should be an array, argument 2 should be a number');
                 }
             })
         })
-        it('Should NOT throw an error if correct argument types are passed', function() {
-            const argPairs = [[[1,2,3,4], 5], [[4,4,4,4], 12], [[], 5], [[44,22,11], 66]]
-            argPairs.forEach((args) => {
-                try {
-                    assert.equal(Array.isArray(indiciesOfSums(args[0], args[1])), true);
-                    
-                } catch(e) {
-                    expect(e.message).to.not.equal('Argument 1 should be an array, argument 2 should be a number');
-                }
-            })
-        })
 
-        // function to check indicies, used in tests below
-        const checkIndicies = (nums, sum, correctLength) => {
-            const indicies = indiciesOfSums(nums, sum);
-            const length = indicies.length;
+        // function to check indices, used in tests below
+        const checkIndices = (nums, sum, correctLength) => {
+            const indices = indicesOfSums(nums, sum);
+            const length = indices.length;
 
             assert.equal(length, correctLength)
             if (length) {
-                indicies.forEach((arr) => {
+                indices.forEach((arr) => {
                     assert.equal(nums[arr[0]] + nums[arr[1]], sum)
                 })
             }
@@ -58,22 +48,22 @@ describe('Robin Healthcate Challenge', function() {
             const sum = 14;
             const length = 4;
 
-            checkIndicies(nums, sum, length)
+            checkIndices(nums, sum, length)
         })
         it('Should pass another case', function() {
             const nums = [-12, 0, -3, 22, 101, 32, 123121, -140];
             const sum = -3;
             const length = 1;
 
-            checkIndicies(nums, sum, length)
+            checkIndices(nums, sum, length)
         })
 
-        it('Should be able to handle the same number repeted in array', function() {
+        it('Should be able to handle the same number repeated in array', function() {
             const nums = [1,1,1,1];
             const sum = 2;
             const length = 6;
 
-            checkIndicies(nums, sum, length)
+            checkIndices(nums, sum, length)
         })
 
         it('Should be able to handle a result that contains no sums', function() {
@@ -81,7 +71,7 @@ describe('Robin Healthcate Challenge', function() {
             const sum = 3;
             const length = 0;
 
-            checkIndicies(nums, sum, length)
+            checkIndices(nums, sum, length)
         })
     })
 })
